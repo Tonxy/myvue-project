c44e7f35a16d3390e54d9bbbe329d7363743c8a2
Tonxy <602663140@qq.com>
2018-09-10 22:43:10 +0800

    文件初始化

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<!-- 展示模板 -->
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+// 导入组件
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components:{
+    HelloWorld
+  }
+}
+</script>
+
+<!-- 样式代码 -->
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
